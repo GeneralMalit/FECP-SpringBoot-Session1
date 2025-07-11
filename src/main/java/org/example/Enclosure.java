b7fb8a92da76aa3a71e2eb6ee58395ecff531ce9
rdpajaron <romielle.pajaron@gcash.com>
2025-07-11 12:15:01 +0800

    "edit building types"

--- src/main/java/org/example/Enclosure.java
+++ src/main/java/org/example/Enclosure.java
@@ -2,18 +2,30 @@ package org.example;
 
 public class Enclosure {
 
-    protected String animalType;
+    private String animalType;
+    private String species;
+
+    public Enclosure(String species, String animalType){
+        this.species = species;
+        this.animalType = animalType;
+    }
 
     class PachydermEnclosure extends Enclosure{
-
+        public PachydermEnclosure(String species, String animalType) {
+            super(species, animalType);
+        }
     }
 
     class FelineEnclosure extends Enclosure{
-
+        public FelineEnclosure(String species, String animalType) {
+            super(species, animalType);
+        }
     }
 
     class BirdEnclosure extends Enclosure{
-
+        public BirdEnclosure(String species, String animalType) {
+            super(species, animalType);
+        }
     }
 
     public String getAnimalType() {
@@ -24,4 +36,11 @@ public class Enclosure {
         this.animalType = animalType;
     }
 
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
 }
