131702da32740cf8815cc327b8bb202bf5abc360
mynt-noir <nuanpatricia.serrano@gcash.com>
2025-07-11 16:12:42 +0800

    add: hospital functions

--- src/main/java/org/example/buildings/Hospital.java
+++ src/main/java/org/example/buildings/Hospital.java
@@ -1,21 +1,37 @@
 package org.example.buildings;
 
+import org.example.animals.Animal;
+import org.example.people.Veterinarian;
+
 public class Hospital {
 
-    void viewSickAnimals(){
+    void viewSickAnimals(Animal[] sickAnimals){
+        System.out.println("Sick Animals Currently in Hospital:");
 
+        for (Animal animal : sickAnimals){
+            System.out.printf("- %s\n", animal.getName());
+        }
     }
 
-    void viewHealedAnimals(){
-
+    void viewHealedAnimals(Animal[] healedAnimals){
+        System.out.println("Healed Animals:");
+        for (Animal animal : healedAnimals){
+            System.out.printf("- %s ✅\n", animal.getName());
+        }
     }
 
-    void attendScienceLecture(){
-
+    void attendScienceLecture(Veterinarian vet){
+        System.out.printf("Dr. %s gives a science lecture on animal health and conservation.\n", vet);
     }
 
-    void healAnimals(){
+    void healAnimals(Animal[] sickAnimals, Animal[] healedAnimals, Veterinarian vet){
+        System.out.printf("Dr. %s begins healing sick animals...\n", vet);
 
+        for (Animal animal : sickAnimals){
+            animal.setHealthy(true);
+            healedAnimals.add(animal.getName());
+            System.out.printf("✅ Healed: %s\n %s has been discharged and returned to enclosure.\n", animal.getName(), animal.getName());
+        }
     }
 
 }
